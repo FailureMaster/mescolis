1b600034735095953bdbc7e6d7be640e068be07b
psmacabulos <107623965+psmacabulos@users.noreply.github.com>
2024-08-09 17:35:42 +0800

    Updated favicon and title

--- src/app/(index)/page.jsx
+++ src/app/(index)/page.jsx
@@ -3,9 +3,8 @@ import { HomeView } from 'src/sections/home/view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimals UI: The starting point for your next project',
-  description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
+  title: 'Mescolis - Express Courrier Shipping',
+  description: 'Best Priced Courrier Service!',
 };
 
 export default function Page() {
