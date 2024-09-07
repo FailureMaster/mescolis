b671cd9b9c4cc17a68e7e6a323a0dc9de97ee5be
Lim Platon <limthai420@gmail.com>
2024-09-08 01:08:24 +0700

    Added modal and added signup link

--- src/sections/how_it_works/career-job-details-hero.js
+++ src/sections/how_it_works/career-job-details-hero.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState, useCallback } from 'react';
+import { m, AnimatePresence } from 'framer-motion';
 
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
@@ -9,6 +10,11 @@ import Checkbox from '@mui/material/Checkbox';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import { alpha, useTheme } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Paper from '@mui/material/Paper';
 
 import { paths } from 'src/routes/paths';
 
@@ -18,6 +24,10 @@ import { bgGradient } from 'src/theme/css';
 
 import Iconify from 'src/components/iconify';
 import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+
+import getVariant from './get-variant'; 
+
+import MarketingContactForm from './marketing-contact-form';
 
 // ----------------------------------------------------------------------
 
@@ -29,6 +39,17 @@ export default function CareerJobDetailsHero({ job }) {
     const handleChangeFavorite = useCallback((event) => {
         setFavorite(event.target.checked);
     }, []);
+
+
+    const [open, setOpen] = useState(false); // State to control the modal
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <Box
@@ -74,7 +95,9 @@ export default function CareerJobDetailsHero({ job }) {
                         sx={{ width: 1, maxWidth: 340 }}
                     >
                         <Stack spacing={2} alignItems="center" sx={{ width: 1 }}>
-                            <Button fullWidth variant="contained" size="large" color="primary">
+                            <Button fullWidth variant="contained" size="large" color="primary"
+                                onClick={handleClickOpen}
+                            >
                                 Try it out. Get a quote.
                             </Button>
                         </Stack>
@@ -82,6 +105,36 @@ export default function CareerJobDetailsHero({ job }) {
 
                 </Stack>
             </Container>
+
+             {/* Modal Implementation */}
+             <AnimatePresence>
+                {open && (
+                    <Dialog
+                        fullWidth
+                        maxWidth="xs"
+                        open={open}
+                        onClose={handleClose}
+                        PaperComponent={(props) => (
+                            <m.div {...getVariant('fadeIn')}>
+                                <Paper {...props}>{props.children}</Paper>
+                            </m.div>
+                        )}
+                    >
+                        <DialogTitle>{`Express Courier Shipping`}</DialogTitle>
+
+                        <DialogContent>
+                            <MarketingContactForm />
+                        </DialogContent>
+
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button variant="contained" onClick={handleClose} autoFocus>
+                                Get Quote
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                )}
+            </AnimatePresence>
         </Box>
     );
 }
